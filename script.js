bedff3aa0bf8ad16d85f609047dd33deb176d514
Fabio  Ramirez <framirez@grupoasesor.net>
2025-02-03 15:56:34 -0600

    codigo

---
+++ script.js
@@ -0,0 +1,268 @@
+"use strict";
+
+function enviar() {
+
+    fetch("https://formsubmit.co/ajax/ramirezrojasfabio@gmail.com", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            Nombre: "Fabio Ramírez Rojas",
+            Email: "fernandazamoraloria@gmail.com",
+            Asunto: "Invitación Formal día Viernes 14 de Febrero a las 3:00 pm",
+            Mensaje: "Buenas tardes, me gustaría invitarla a una cita el día viernes 14 de febrero a las 3:00 pm en la que realizaremos actividades que no se pueden revelar ya que son de caracter confidencial. Tenemos 3 actividades que sé que te encantarán!"
+        })
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        // .then(window.location.href = "../index.html")
+        .catch(error => console.log(error));
+
+}
+
+
+let noButtonClickCount = 0; 
+let noButtonState = 0;
+
+document.getElementById('gifContainer').style.display = 'block';
+
+document.getElementById('siBtn').addEventListener('click', function() {
+    document.getElementById('sadGifContainer').style.display = 'none';
+    document.getElementById('sadGifContainer1').style.display = 'none';
+    document.getElementById('sadGifContainer2').style.display = 'none';
+    document.getElementById('gifContainer').style.display = 'none';
+    document.getElementById('happyGifContainer').style.display = 'block';
+
+    document.getElementById('question').style.display = 'none';
+    document.getElementById('siBtn').style.display = 'none';
+    document.body.classList.add('bg-green');
+    document.getElementById('noBtn').style.display = 'none';
+
+    document.getElementById('messageContainer').style.display = 'block';
+    document.getElementById('messageContainer').innerHTML = 'TE AMO, LA INVITACIÓN HA SIDO ENVIADA VIA EMAIL';
+
+    setTimeout(function() {
+        document.getElementById('happyGifContainer').style.display = 'none';
+        document.getElementById('happyGifContainer2').style.display = 'block';
+    }, 1000);
+
+    setTimeout(function() {
+        document.getElementById('happyGifContainer2').style.display = 'none';
+        document.getElementById('happyGifContainer3').style.display = 'block';
+    }, 2000);
+    setTimeout(function() {
+        document.getElementById('happyGifContainer3').style.display = 'none';
+        document.getElementById('happyGifContainer4').style.display = 'block';
+    }, 3000);
+  enviar();
+});
+
+
+document.getElementById('noBtn').addEventListener('click', function() {
+    switch (noButtonState) {
+        case 0:
+            document.getElementById('happyGifContainer').style.display = 'none';
+            document.getElementById('gifContainer').style.display = 'none';
+            document.getElementById('sadGifContainer').style.display = 'block';
+
+            document.getElementById('noBtn').innerHTML = 'ehhh, segura?';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+          
+            
+            document.getElementById('siBtn').style.fontSize = '40px';
+            document.getElementById('siBtn').style.padding = '20px 40px';
+       
+            
+           
+
+        
+            noButtonClickCount++;
+            noButtonState++;
+            break;
+
+        case 1:
+
+            document.getElementById('noBtn').innerHTML = '100% Segura?';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+            document.getElementById('sadGifContainer').style.display = 'none';
+            document.getElementById('sadGifContainer2').style.display = 'block';
+
+            document.getElementById('siBtn').style.fontSize = '50px';
+            document.getElementById('siBtn').style.padding = '30px 50px';
+        
+            noButtonState++;
+            break;
+
+        case 2:
+           
+            document.getElementById('noBtn').innerHTML = 'Segura segura segurísima?';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+            document.getElementById('sadGifContainer').style.display = 'none';
+            document.getElementById('sadGifContainer2').style.display = 'none';
+            document.getElementById('sadGifContainer1').style.display = 'block';
+
+            document.getElementById('siBtn').style.fontSize = '60px';
+            document.getElementById('siBtn').style.padding = '40px 60px';
+        
+            noButtonState++;
+            break;
+        
+        case 3:
+           
+            document.getElementById('noBtn').innerHTML = '100000% SEGURA?';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            document.getElementById('siBtn').style.fontSize = '70px';
+            document.getElementById('siBtn').style.padding = '50px 70px';
+        
+            noButtonState++;
+            break;
+        case 4:
+           
+            document.getElementById('noBtn').innerHTML = 'POR FAVOR DIGA QUE SÍ🥹🥹?';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            document.getElementById('siBtn').style.fontSize = '80px';
+            document.getElementById('siBtn').style.padding = '60px 80px';
+         
+            noButtonState++;
+            break;
+        case 5:
+           
+            document.getElementById('noBtn').innerHTML = 'PIÉNSALO UN POCO';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            document.getElementById('siBtn').style.fontSize = '90px';
+            document.getElementById('siBtn').style.padding = '70px 90px';
+
+            noButtonState++;
+            break;
+        case 6:
+           
+            document.getElementById('noBtn').innerHTML = 'POR QUÉEEEE NOOOO?';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            document.getElementById('siBtn').style.fontSize = '100px';
+            document.getElementById('siBtn').style.padding = '80px 100px';
+            
+            noButtonState++;
+            break;
+        case 7:
+           
+            document.getElementById('noBtn').innerHTML = 'Awww💔💔';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            document.getElementById('siBtn').style.fontSize = '120px';
+            document.getElementById('siBtn').style.padding = '90px 120px';
+  
+            noButtonState++;
+            break;
+        case 8:
+           
+            document.getElementById('noBtn').innerHTML = 'PORFIIIIIIIIIIIS🤧🤧';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            document.getElementById('siBtn').style.fontSize = '140px';
+            document.getElementById('siBtn').style.padding = '100px 140px';
+       
+            noButtonState++;
+            break;
+
+        case 9:
+           
+            document.getElementById('noBtn').innerHTML = 'DI QUE SÍ, DI QUE SÍ😉';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            document.getElementById('siBtn').style.fontSize = '160px';
+            document.getElementById('siBtn').style.padding = '110px 160px';
+          
+            noButtonState++;
+            break;
+
+        case 10:
+           
+            document.getElementById('noBtn').innerHTML = 'Listo, ya entendí...'
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+         
+            document.getElementById('siBtn').style.fontSize = '180px';
+            document.getElementById('siBtn').style.padding = '120px 180px';
+    
+            noButtonState++;
+            break;
+        case 11:
+           
+            document.getElementById('noBtn').innerHTML = 'bromaaaaaa, diga que siiiiiiii😫😫';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            document.getElementById('siBtn').style.fontSize = '200px';
+            document.getElementById('siBtn').style.padding = '130px 200px';
+            
+            noButtonState++;
+            break;
+        
+        case 12:
+           
+            document.getElementById('noBtn').innerHTML = 'Es un poco feo decir que no, no?';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            document.getElementById('siBtn').style.fontSize = '220px';
+            document.getElementById('siBtn').style.padding = '140px 220px';
+    
+            noButtonState++;
+            break;
+        
+        case 13:
+           
+            document.getElementById('noBtn').innerHTML = 'POR QUÉEEE NOOOOO :(';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            document.getElementById('siBtn').style.fontSize = '240px';
+            document.getElementById('siBtn').style.padding = '150px 240px';
+         
+            noButtonState++;
+            break;
+        
+        case 14:
+           
+            document.getElementById('noBtn').innerHTML = 'YA DIJE QUE NO PERRO HIJUEPUTA!';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            document.getElementById('siBtn').style.fontSize = '260px';
+            document.getElementById('siBtn').style.padding = '160px 260px';
+            
+            noButtonState++;
+            break;
+        
+        case 15:
+           
+            document.getElementById('noBtn').innerHTML = 'VAMOS TÚ PUEDES DECIR QUE SÍ';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            document.getElementById('siBtn').style.fontSize = '280px';
+            document.getElementById('siBtn').style.padding = '170px 280px';
+          
+            noButtonState++;
+            break;
+        
+     
+        case 16:
+                document.getElementById('noBtn').innerHTML = 'POR FAVORCITO, VAMOS DE NUEVO😔';
+                document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+              
+                document.getElementById('sadGifContainer').style.display = 'none';
+                document.getElementById('sadGifContainer1').style.display = 'none';
+                document.getElementById('sadGifContainer2').style.display = 'none';
+                document.getElementById('gifContainer').style.display = 'block';
+                document.getElementById('happyGifContainer').style.display = 'none';
+                noButtonState = 0;
+                break;
+        
+
+        default:
+            break;
+    }
+});
+
